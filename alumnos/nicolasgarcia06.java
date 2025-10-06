8d77d7e0b7607590d0630bfcd3ff48a115ce0940
nicolasgarcia06 <805@alumnos.fpsumma.es>
2025-10-06 08:59:51 +0200

    formulario relleno

--- alumnos/nicolasgarcia06.java
+++ alumnos/nicolasgarcia06.java
@@ -4,12 +4,12 @@ public class nicolasgarcia06 {
 # Ficha técnica – Git y GitHub Desktop
 
 ## Mis datoss
-**Alumno/a:** <tu nombre> — **Usuario GitHub:** @<tu-usuario>
+**Alumno/a:**Nicolas — **Usuario GitHub:** @nicolasgarcia06
 
 ## Conceptos en tus palabras
-- **Fork:** 
-- **Branch:** 
-- **Pull Request:**
-- **Diferencia entre fork y branch:** 
-- **Cuándo abrir un PR:** 
+- **Fork:** Un fork es una copia de un repositorio que sirve para proponer mejoras del repositorio original pero sin modificarlo
+- **Branch:**  Un branch es una rama en la que puedes hacer cambios sin afectar a la original
+- **Pull Request:**sirve para que los cambios hechos de una rama (branch) se suban a la rama principal(main)
+- **Diferencia entre fork y branch:** Un fork es una copia de un repositorio en mi cuenta para trabajar de forma independiente y  un branch es una rama dentro del mismo repositorio para hacer cambios que no afectan al original
+- **Cuándo abrir un PR:** Es cuando hago una rama y quiero subir los cambios de esa rama a la rama original para que los demas vean mi trabajo 
 }
